c407b13d600fd33be88dad8df752eee08cc76ec6
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-05-07 02:15:29 -0300

    .

--- R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/SetLinkedListImpl.java
+++ R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/SetLinkedListImpl.java
@@ -5,66 +5,75 @@ import adt.linkedList.SingleLinkedListNode;
 
 public class SetLinkedListImpl<T> extends SingleLinkedListImpl<T> implements SetLinkedList<T> {
 
-   @Override
-   public void removeDuplicates() {
-      SingleLinkedListNode<T> tempHead = this.getHead();
+	@Override
+	public void removeDuplicates() {
+		SingleLinkedListNode<T> tempHead = this.getHead();
 
-      if (isEmpty()){return;}
+		if (isEmpty()) {
+			return;
+		}
 
-      while (!tempHead.isNIL()) {
-         SingleLinkedListNode<T> iNode = tempHead.getNext();
+		while (!tempHead.isNIL()) {
+			SingleLinkedListNode<T> iNode = tempHead.getNext();
 
-         while (!iNode.isNIL()) {
-            if (tempHead.getData().equals(iNode.getData()))
-               iNode.setData(null);
-            iNode = iNode.getNext();
-         }
-         tempHead = tempHead.getNext();
-      }
-   }
+			while (!iNode.isNIL()) {
+				if (tempHead.getData().equals(iNode.getData()))
+					iNode.setData(null);
+				iNode = iNode.getNext();
+			}
+			tempHead = tempHead.getNext();
+		}
+	}
 
-   @Override
-   public SetLinkedList<T> union(SetLinkedList<T> otherSet) {
-   		if (this.isEmpty()) return otherSet;
- 	  	else if (otherSet.isEmpty()) return this;
+	@Override
+	public SetLinkedList<T> union(SetLinkedList<T> otherSet) {
+		if (this.isEmpty())
+			return otherSet;
+		else if (otherSet.isEmpty())
+			return this;
 
- 	  	SingleLinkedListNode auxHead = this.getHead();
- 	  	while (!auxHead.isNIL()) {
- 	  		otherSet.insert((T) auxHead.getData());
- 	  		auxHead = auxHead.getNext();
- 	  	}
+		SingleLinkedListNode auxHead = this.getHead();
+		while (!auxHead.isNIL()) {
+			otherSet.insert((T) auxHead.getData());
+			auxHead = auxHead.getNext();
+		}
 
-      	return otherSet;
-   }
+		return otherSet;
+	}
 
-   @Override
-   public SetLinkedList<T> intersection(SetLinkedList<T> otherSet) {
-	   if (this.isEmpty()) return otherSet;
-	   else if (otherSet.isEmpty()) return this;
+	@Override
+	public SetLinkedList<T> intersection(SetLinkedList<T> otherSet) {
+		if (this.isEmpty())
+			return otherSet;
+		else if (otherSet.isEmpty())
+			return this;
 
-	   SingleLinkedListNode auxHead = this.getHead();
-	   SetLinkedList<T> intersecSet = new SetLinkedListImpl<>();
+		SingleLinkedListNode auxHead = this.getHead();
+		SetLinkedList<T> intersecSet = new SetLinkedListImpl<>();
 
-       while (!auxHead.isNIL()) {
-          if (otherSet.search((T) auxHead.getData()) != null)
-          	intersecSet.insert((T) auxHead.getData());
-          auxHead = auxHead.getNext();
-       }
+		while (!auxHead.isNIL()) {
+			if (otherSet.search((T) auxHead.getData()) != null)
+				intersecSet.insert((T) auxHead.getData());
+			auxHead = auxHead.getNext();
+		}
 
-       return intersecSet;
-   }
+		return intersecSet;
+	}
 
-   @Override
-   public void concatenate(SetLinkedList<T> otherSet) {
-	   if (this.isEmpty() || otherSet.isEmpty()) return ;
+	@Override
+	public void concatenate(SetLinkedList<T> otherSet) {
+		if (this.isEmpty() || otherSet.isEmpty())
+			return;
 
-	   SingleLinkedListNode<T> auxHead = this.getHead();
+		SingleLinkedListNode<T> auxHead = this.getHead();
 
-	   while (!auxHead.getNext().isNIL()) { auxHead = auxHead.getNext(); }
+		while (!auxHead.getNext().isNIL()) {
+			auxHead = auxHead.getNext();
+		}
 
-	   SingleLinkedListNode<T> auxHead2 = ((SingleLinkedListImpl<T>) otherSet).getHead();
+		SingleLinkedListNode<T> auxHead2 = ((SingleLinkedListImpl<T>) otherSet).getHead();
 
-	   auxHead.setNext(auxHead2);
-   }
+		auxHead.setNext(auxHead2);
+	}
 
 }
--- R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/StudentSetLinkedListTest.java
+++ R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/StudentSetLinkedListTest.java
@@ -6,60 +6,60 @@ import org.junit.Test;
 
 public class StudentSetLinkedListTest {
 
-   protected SetLinkedListImpl<Integer> lista1;
-   protected SetLinkedListImpl<Integer> lista2;
+	protected SetLinkedListImpl<Integer> lista1;
+	protected SetLinkedListImpl<Integer> lista2;
 
-   @Before
-   public void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 
-      getImplementations();
+		getImplementations();
 
-      lista1.insert(1);
-      lista1.insert(2);
-      lista1.insert(3);
-      lista1.insert(3);
-      lista1.insert(3);
-      lista1.insert(2);
+		lista1.insert(1);
+		lista1.insert(2);
+		lista1.insert(3);
+		lista1.insert(3);
+		lista1.insert(3);
+		lista1.insert(2);
 
-      lista2.insert(1);
-      lista2.insert(4);
-      lista2.insert(5);
-   }
+		lista2.insert(1);
+		lista2.insert(4);
+		lista2.insert(5);
+	}
 
-   private void getImplementations() {
-      lista1 = new SetLinkedListImpl<>();
-      lista2 = new SetLinkedListImpl<>();
-   }
+	private void getImplementations() {
+		lista1 = new SetLinkedListImpl<>();
+		lista2 = new SetLinkedListImpl<>();
+	}
 
-   @Test
-   public void testConcatenate() {
-      Assert.assertArrayEquals(new Integer[] { 1, 4, 5 }, lista2.toArray());
-      Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
-      lista1.concatenate(lista2);
-      Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2, 1, 4, 5 }, lista1.toArray());
-   }
+	@Test
+	public void testConcatenate() {
+		Assert.assertArrayEquals(new Integer[] { 1, 4, 5 }, lista2.toArray());
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
+		lista1.concatenate(lista2);
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2, 1, 4, 5 }, lista1.toArray());
+	}
 
-   @Test
-   public void testRemoveD() {
-      Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
-      lista1.removeDuplicates();
-      Assert.assertArrayEquals(new Integer[] { 1, 2, 3 }, lista1.toArray());
-   }
+	@Test
+	public void testRemoveD() {
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
+		lista1.removeDuplicates();
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3 }, lista1.toArray());
+	}
 
-   @Test
-   public void testUnion() {
-      Assert.assertArrayEquals(new Integer[] { 1, 4, 5 }, lista2.toArray());
-      Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
-      SetLinkedList<Integer> a = lista1.union(lista2);
-      Assert.assertArrayEquals(new Integer[] { 1, 4, 5, 1, 2, 3, 3, 3, 2 }, a.toArray());
-   }
+	@Test
+	public void testUnion() {
+		Assert.assertArrayEquals(new Integer[] { 1, 4, 5 }, lista2.toArray());
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
+		SetLinkedList<Integer> a = lista1.union(lista2);
+		Assert.assertArrayEquals(new Integer[] { 1, 4, 5, 1, 2, 3, 3, 3, 2 }, a.toArray());
+	}
 
-   @Test
-   public void testIntersection() {
-      Assert.assertArrayEquals(new Integer[] { 1, 4, 5 }, lista2.toArray());
-      Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
-      SetLinkedList<Integer> a = lista1.intersection(lista2);
-      Assert.assertArrayEquals(new Integer[] { 1 }, a.toArray());
-   }
+	@Test
+	public void testIntersection() {
+		Assert.assertArrayEquals(new Integer[] { 1, 4, 5 }, lista2.toArray());
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 3, 3, 2 }, lista1.toArray());
+		SetLinkedList<Integer> a = lista1.intersection(lista2);
+		Assert.assertArrayEquals(new Integer[] { 1 }, a.toArray());
+	}
 
 }
