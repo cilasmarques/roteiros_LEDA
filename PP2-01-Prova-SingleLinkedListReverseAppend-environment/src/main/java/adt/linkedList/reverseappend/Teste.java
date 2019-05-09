6f97790ed796a84a01421e4aef69fc0fc1b90a83
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-05-09 17:16:09 -0300

    .

--- PP2-01-Prova-SingleLinkedListReverseAppend-environment/src/main/java/adt/linkedList/reverseappend/Teste.java
+++ PP2-01-Prova-SingleLinkedListReverseAppend-environment/src/main/java/adt/linkedList/reverseappend/Teste.java
@@ -12,6 +12,15 @@ public class Teste {
         reverseA.doIt(1);
         reverseA.doIt(2);
         reverseA.doIt(3);
+        reverseA.doIt(4);
+        reverseA.doIt(5);
+        reverseA.doIt(6);
+        reverseA.doIt(7);
+        reverseA.doIt(8);
+        reverseA.doIt(9);
+        reverseA.doIt(10);
+        reverseA.doIt(11);
+        reverseA.doIt(12);
 
         SingleLinkedListNode aux2 = reverseA.head;
 
