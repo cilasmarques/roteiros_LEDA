a0a5db6251fb1c834988c897c4da59fc85b302c9
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-06-13 21:47:58 -0300

    .

---
+++ R18-01-Rot-ArvoreB-environment/src/main/java/adt/btree/BTreeImpl.java
@@ -0,0 +1,99 @@
+package adt.btree;
+
+public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
+
+	protected BNode<T> root;
+	protected int order;
+
+	public BTreeImpl(int order) {
+		this.order = order;
+		this.root = new BNode<T>(order);
+	}
+
+	@Override
+	public BNode<T> getRoot() {
+		return this.root;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.root.isEmpty();
+	}
+
+	@Override
+	public int height() {
+		return height(this.root);
+	}
+
+	private int height(BNode<T> node) {
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	@Override
+	public BNode<T>[] depthLeftOrder() {
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	@Override
+	public int size() {
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	@Override
+	public BNodePosition<T> search(T element) {
+		if (element != null){
+			return search(getRoot(), element);
+		}
+		return null;
+	}
+
+	private BNodePosition<T> search(BNode<T> node, T element) {
+		int i = 1;
+		while (i <= node.size() && element.compareTo(node.elements.get(i)) > 0 ) {i++;}
+		if (i < node.size() && element.equals(node.elements.get(i)))
+			return new BNodePosition<>(node,i);
+		if (node.isLeaf())
+			return new BNodePosition<>();
+
+		return search(node.getChildren().get(i), element);
+	}
+
+	@Override
+	public void insert(T element) {
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	private void split(BNode<T> node) {
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	private void promote(BNode<T> node) {
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	// NAO PRECISA IMPLEMENTAR OS METODOS ABAIXO
+	@Override
+	public BNode<T> maximum(BNode<T> node) {
+		// NAO PRECISA IMPLEMENTAR
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	@Override
+	public BNode<T> minimum(BNode<T> node) {
+		// NAO PRECISA IMPLEMENTAR
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+	@Override
+	public void remove(T element) {
+		// NAO PRECISA IMPLEMENTAR
+		throw new UnsupportedOperationException("Not Implemented yet!");
+	}
+
+}
