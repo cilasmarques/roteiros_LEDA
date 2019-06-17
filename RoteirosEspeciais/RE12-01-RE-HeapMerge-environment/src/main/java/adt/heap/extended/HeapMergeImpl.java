73deeda387fc05ae049becb2a80676a05e87c777
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-06-17 04:07:49 -0300

    .

--- RoteirosEspeciais/RE12-01-RE-HeapMerge-environment/src/main/java/adt/heap/extended/HeapMergeImpl.java
+++ RoteirosEspeciais/RE12-01-RE-HeapMerge-environment/src/main/java/adt/heap/extended/HeapMergeImpl.java
@@ -1,5 +1,8 @@
 package adt.heap.extended;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,14 +16,37 @@ public class HeapMergeImpl extends HeapImpl<Integer> implements HeapMerge {
 
 	@Override
 	public Integer[] mergeArraysAndOrder(List<Integer[]> arrays) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		for (Integer[] array: arrays){
+			for (int i = 0; i < array.length; i ++){
+				insert(array[i]);
+			}
+		}
+		return heapsort(heap);
 	}
+
+	/*
+	@Override
+	public Integer[] mergeArraysAndOrder(List<Integer[]> arrays) {
+		ArrayList mergeArrays = new ArrayList();
+		for (Integer[] array: arrays){
+			mergeArrays.add(array);
+		}
+		Collections.sort(mergeArrays);
+		return (Integer[]) mergeArrays.toArray();
+	}
+	*/
 
 	@Override
 	public int sumRange(int k1, int k2) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		this.comparator = Comparator.reverseOrder();
+		Integer[] array = heapsort(this.heap);
+		int sumRange = 0;
+		for (int i = heap.length - 1; i > k1; i--){
+			if (i < k2)
+				sumRange += array[i];
+		}
+		return sumRange;
 	}
 
 }
